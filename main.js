ad0753f04528e2b8c7e125cf4b81916ea50c172b
tahara <a@a.com>
2015-10-04 22:18:55 +0900

    Y軸のmax値を設定

--- main.js
+++ main.js
@@ -1,31 +1,47 @@
 $(function() {
 	// でんこステータスデータを取得
 	$.getJSON("denko.json",function(json){
+		// APのグラフ設定
 		$('#ap_graph').highcharts({
 			title : {text: "AP比較表"},
-			xAxis : {title: {text: "レベル"},
+			xAxis : {
+				title: {text: "レベル"},
+				min:0,
+				max:80,
 				plotLines: [{
 					value: 50,
 					color: 'red',
-					width: 2
+					width: 2,
 				}]
 			},
-			yAxis : {title: {text: "数値"},min:0,max:400},
+			yAxis : {
+				title: {text: "数値"},
+				min:0,
+				max:400
+			}
 		});
 		var ap_chart = $('#ap_graph').highcharts();
 		$.each(json, function(i, val){
 			ap_chart.addSeries({name: val.name, data: val.AP, visible:false});
 		});
+		// HPのグラフ設定
 		$('#hp_graph').highcharts({
 			title : {text: "HP比較表"},
-			xAxis : {title: {text: "レベル"},
+			xAxis : {
+				title: {text: "レベル"},
+				min:0,
+				max:80,
 				plotLines: [{
 					value: 50,
 					color: 'red',
-					width: 2
+					width: 2,
 				}]
 			},
-			yAxis : {title: {text: "数値"},min:0,max:500}
+			yAxis : {
+				title: {text: "数値"},
+				min:0,
+				max:500
+			}
 		});
 		var hp_chart = $('#hp_graph').highcharts();
 		$.each(json, function(i, val){
