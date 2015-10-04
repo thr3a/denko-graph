9104a43751ff9f1fc05a51804e02f528f6c47aee
tahara <a@a.com>
2015-10-04 21:50:56 +0900

    initial commit

---
+++ main.js
@@ -0,0 +1,35 @@
+$(function() {
+	// でんこステータスデータを取得
+	$.getJSON("denko.json",function(json){
+		$('#ap_graph').highcharts({
+			title : {text: "AP比較表"},
+			xAxis : {title: {text: "レベル"},
+				plotLines: [{
+					value: 50,
+					color: 'red',
+					width: 2
+				}]
+			},
+			yAxis : {title: {text: "数値"},min:0,max:400},
+		});
+		var ap_chart = $('#ap_graph').highcharts();
+		$.each(json, function(i, val){
+			ap_chart.addSeries({name: val.name, data: val.AP, visible:false});
+		});
+		$('#hp_graph').highcharts({
+			title : {text: "HP比較表"},
+			xAxis : {title: {text: "レベル"},
+				plotLines: [{
+					value: 50,
+					color: 'red',
+					width: 2
+				}]
+			},
+			yAxis : {title: {text: "数値"},min:0,max:500}
+		});
+		var hp_chart = $('#hp_graph').highcharts();
+		$.each(json, function(i, val){
+			hp_chart.addSeries({name: val.name, data: val.HP, visible:false});
+		});
+	});
+});
